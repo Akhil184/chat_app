b3cdfe4e84a6413cb35adf529abe1e482479f9b0
siddharthteli12 <akhilteli55@gmail.com>
2024-05-19 16:26:06 +0530

    create slpash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+
+
 
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
@@ -14,17 +16,56 @@ class _SplashPageState extends State<SplashPage> {
   @override
   void initState() {
     super.initState();
-    Timer(Duration(seconds: 3),
-            ()=>Navigator.pushReplacement(context,
-            MaterialPageRoute(builder:
-                (context) =>
-                SecondScreen()
+    Timer(Duration(seconds: 5),
+            () =>
+            Navigator.pushReplacement(context,
+                MaterialPageRoute(builder:
+                    (context) => HomeScreen()
+                )
             )
-        )
     );
   }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+        color: Colors.blue,
+        child:Image.asset('assets/images/images.jpeg',height: MediaQuery
+            .of(context)
+            .size
+            .height)
+    );
   }
 }
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor:Colors.blueAccent,
+      appBar: AppBar(
+        backgroundColor:Colors.purple,
+        title: Text("Login Page",style:TextStyle(color:Colors.white),),centerTitle:true,),
+      body:Center(
+
+      child:Container(
+        height:400,
+      child:Card(
+
+        elevation: 50,
+        shadowColor: Colors.black,
+        color: Colors.white,
+      child:Center(
+          child:TextField(
+
+          )
+      ),
+      ),
+      ),),
+    );
+  }
+}
+
+
+
+
